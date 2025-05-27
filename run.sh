8280145d79d06540002df73257a00edbb4ff4d2b
mshagirov <muratshagirov@gmail.com>
2025-05-27 16:21:50 +0800

    option for running in a background

--- run.sh
+++ run.sh
@@ -4,17 +4,19 @@ Help()
 {
   # Display Help
   echo "Run API server at a given port (default port 8000)"
-  echo ""
-  echo "Usage: $0 [-p PORT] [-i HOST]"
-  echo "options:"
-  echo "-h           Print this help." 
+  echo
+  echo "Usage: $0 [-p PORT] [-i HOST] [-b]"
+  echo
+  echo "Options:"
+  echo "-h          Print this help" 
+  echo "-b          Run in a background, must be used with '&', e.g. 'nohup ./run.sh -b &'"
   echo "-p <PORT>   Port number (int) to use instead of default 8000"
   echo "-i <HOST>   Host IP address, defaults to 0.0.0.0 (all interfaces)"
+  echo 
+  echo "NOTE: Don not forget to close the program when running in the background."
 }
 
-# if [[ "$#" -lt 1 ]]; then PORT=8000; fi
-
-while getopts ":hp:i:" option; do
+while getopts ":hp:i:b" option; do
   case $option in 
     h) #help
       Help
@@ -25,6 +27,9 @@ while getopts ":hp:i:" option; do
       ;;
     i) #host address
       HOST=${OPTARG}
+      ;;
+    b) #run in a background
+      BACKGROUND="True"
       ;;
     *) #invalid options
       Help
@@ -39,4 +44,9 @@ if [[ $HOST == "" ]]; then HOST="0.0.0.0"; fi
 
 echo HOST:PORT = $HOST:$PORT
 
-fastapi run --port $PORT --host $HOST mm/api.py
+if [[ $BACKGROUND != "" ]] ; then
+    $0 -p ${PORT} -i ${HOST} </dev/null &>/dev/null
+else
+    fastapi run --port $PORT --host $HOST mm/api.py
+fi
+
