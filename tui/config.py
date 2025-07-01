02b359b4d4112e0e56fb130ed7b7759eeacb8fb7
mshagirov <muratshagirov@gmail.com>
2025-07-01 14:37:17 +0800

    tui for remote monitoring

---
+++ tui/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import yaml
+
+
+def get_list(path_to_file: str | Path ='machine-list.yaml'):
+    """
+    path_to_file : location of YAML configuration file
+    """
+    if not isinstance(path_to_file, Path):
+        path_to_file = Path(path_to_file)
+    
+    if not path_to_file.is_file():
+        raise(FileNotFoundError)
+    
+    with open(path_to_file, 'r') as f:
+        content = yaml.safe_load(f)
+    if not isinstance(content, dict):
+        raise(ValueError(f"YAML should contain 'monitor' key with machines and their URLs following format below:\nmonitor:\n  NAME: URL"))
+    return content
---
+++ tui/tui.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+import re
+import sys, os
+import requests
+
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+from textual.binding import Binding
+
+from config import get_list
+
+exp_char_ = "┣"
+col_char_ = "╠"
+
+def get_info(hostname : str):
+    try:
+        res = requests.get("http://" + hostname + "/info", headers={"accept": "application/json"})
+        return res.json()
+    except Exception as e:
+        return {'error':str(e)}
+
+def get_metrics(hostname : str):
+    try:
+        res = requests.get("http://" + hostname + "/metrics", headers={"accept": "application/json"})
+        return res.json()
+    except Exception as e:
+        return {'error':str(e)}
+
+def valid_list(nodes):
+    if not isinstance(nodes.get('monitor'), dict):
+        return False
+    if len(nodes.get('monitor')) < 1:
+        return False
+    return True
+
+class MachineMonitor(App):
+    """A Terminal app to monitor machines over the network via requests API"""
+
+    # Key bindings
+    BINDINGS = [('d', 'toggle_dark', 'Toggle dark mode'),
+                Binding('q', 'quit', 'Quit', show=False, priority=True)]
+
+    def compose(self) -> ComposeResult:
+        yield Header(name='Machines', show_clock=True)
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        path_to_list = sys.argv[1]
+    else:
+        path_to_list = Path(__file__).resolve().parent / "list.yaml"
+
+    try:
+        nodes = get_list(path_to_list)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    if not valid_list(nodes):
+        print(f"Monitoring list is empty:\n\t{path_to_list}")
+        sys.exit(1)
+
+    for node_name, url in nodes['monitor'].items():
+        print(node_name)
+        print(get_info(url))
+        print(get_metrics(url))
+    # app = MachineMonitor()
+    # app.run()
+
